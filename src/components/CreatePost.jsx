7d6eb775e4d5d1f1fcd3bacbf0aab2ffa3b2335a
Priyanka <priyanka2001@gmail.com>
2025-09-30 08:01:42 +0530

    49 Route Links

--- src/components/CreatePost.jsx
+++ src/components/CreatePost.jsx
@@ -1,8 +1,10 @@
 import { useContext, useRef } from "react";
 import { PostList } from "../store/post-list-store";
+import { useNavigate } from "react-router-dom";
 
 const CreatePost = () => {
   const { addPost } = useContext(PostList);
+  const navigate = useNavigate();
 
   const userIdElement = useRef();
   const postTitleElement = useRef();
@@ -45,16 +47,8 @@ const CreatePost = () => {
       .then((res) => res.json())
       .then((post) => {
         addPost(post);
+        navigate("/")
       });
-
-    // addPost(
-    //   userId,
-    //   postTitle,
-    //   recationsLikes,
-    //   recationsDislike,
-    //   postBody,
-    //   tags
-    // );
   };
 
   return (
